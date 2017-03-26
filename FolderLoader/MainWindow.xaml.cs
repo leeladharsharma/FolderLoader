983c6411756cb95f70328e1a6081886499e228f0
Leeladhar Sharma <Leeladhar Sharma>
2017-03-26 17:20:42 -0400

    First commit

---
+++ FolderLoader/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FolderLoader.ViewModel;
+
+namespace FolderLoader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Variables and Properties
+
+        private object dummyNode = null;
+
+        private long value = 0;
+
+        public string SelectedImagePath { get; set; }
+
+        #endregion
+
+        #region Methods and Events
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Store the size specified by the user and disable button
+        /// </summary>      
+        private void ShowBasedOnSizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            value = long.Parse(FileSize.Text);
+            btnShow.IsEnabled = false;
+
+        }
+
+ 
+        /// <summary>
+        /// Load the Drives in the system when application loads
+        /// </summary>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                try
+                {
+                    TreeViewItem item = new TreeViewItem();
+                    DriveInfo info = new DriveInfo(s);
+                    double usedSize = info.TotalFreeSpace;
+                    item.Header = s + "          " + usedSize + " bytes"; ;
+                    item.Tag = s;
+                    item.FontWeight = FontWeights.Normal;
+                    item.Items.Add(dummyNode);
+                    item.Expanded += new RoutedEventHandler(folder_Expanded);
+                    foldersItem.Items.Add(item);
+                }
+                catch (Exception) { }
+
+            }
+
+        }
+
+        /// <summary>
+        /// Async event that displays subfolders when folders expanded
+        /// </summary>
+        async void folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            btnShow.IsEnabled = false;
+            TreeViewItem item = (TreeViewItem)sender;
+
+            if (item.Items.Count == 1 && item.Items[0] == dummyNode)
+            {
+                item.Items.Clear();
+                try
+                {
+                    foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+                    {
+                        await Task.Run(() => FolderViewModel.CalculateFolderSize(s));
+                        try
+                        {
+                            TreeViewItem subitem = new TreeViewItem();
+
+                            string subTemp = s.Substring(s.LastIndexOf("\\") + 1);
+                            FileInfo info = new FileInfo(s);
+
+                            long usedSize = FolderViewModel.CalculateFolderSize(s);
+                            if (usedSize > value)
+                            {
+                                subitem.Header = subTemp + "          " + usedSize + " bytes";
+                                subitem.Tag = s;
+                                subitem.FontWeight = FontWeights.Normal;
+                                subitem.Items.Add(dummyNode);
+                                subitem.Expanded += new RoutedEventHandler(folder_Expanded);
+                                item.Items.Add(subitem);
+                            }
+                        }
+                        catch (Exception) { }
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+
+
+        /// <summary>
+        /// Loads the image from header when item selection is changed
+        /// </summary>
+        private void foldersItem_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            TreeView tree = (TreeView)sender;
+            TreeViewItem temp = ((TreeViewItem)tree.SelectedItem);
+
+            if (temp == null)
+                return;
+            SelectedImagePath = "";
+            string temp1 = "";
+            string temp2 = "";
+            while (true)
+            {
+                temp1 = temp.Header.ToString();
+                if (temp1.Contains(@"\"))
+                {
+                    temp2 = "";
+                }
+                SelectedImagePath = temp1 + temp2 + SelectedImagePath;
+                if (temp.Parent.GetType().Equals(typeof(TreeView)))
+                {
+                    break;
+                }
+                temp = ((TreeViewItem)temp.Parent);
+                temp2 = @"\";
+            }
+
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ FolderLoader/ViewModel/FolderViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderLoader.ViewModel
+{
+    public class FolderViewModel
+    {
+        #region Methods
+        
+        /// <summary>
+        /// Calculates and returns the size of subfolders 
+        /// </summary>
+        public static long CalculateFolderSize(string folder)
+        {
+            long folderSize = 0;
+            try
+            {
+                //Checking if the path is valid or not
+                if (!Directory.Exists(folder))
+                    return folderSize;
+                else
+                {
+                    try
+                    {
+                        foreach (string file in Directory.GetFiles(folder))
+                        {
+                            if (File.Exists(file))
+                            {
+                                FileInfo finfo = new FileInfo(file);
+                                folderSize += finfo.Length;
+                            }
+                        }
+
+                        foreach (string dir in Directory.GetDirectories(folder))
+                            folderSize += CalculateFolderSize(dir);
+                    }
+                    catch (NotSupportedException e)
+                    {
+                        Console.WriteLine("Failed to calculate folder size: {0}", e.Message);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Failed to calculate folder size: {0}", e.Message);
+            }
+            return folderSize;
+        }
+
+        #endregion
+    }
+}
